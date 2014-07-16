628f408b81a02199bdfb6486a4d65406de60a201
jgaviria <jgaviria@Juan-Gaviria.local>
2014-07-16 18:03:56 -0400

    sd

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :budgets
 
   resources :products
-  root 'budget#new'
+
+  root 'budgets#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140716182845) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "budgets", force: true do |t|
     t.decimal  "income"
     t.decimal  "savings"
